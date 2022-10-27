6a9c1edca8fb988dbd410a1bb7758a4329c7d22f
tylernguyen <tnguyen28@luc.edu>
2022-10-27 17:09:16 -0500

    change experience tab list order

--- client/src/components/Experience/Tabs/constants.ts
+++ client/src/components/Experience/Tabs/constants.ts
@@ -3,41 +3,6 @@ import { ExperienceData } from "./types";
 
 export const experienceData: ExperienceData[] = [
   {
-    title: "Mobile Application Developer Intern",
-    companyName: "Robert Bosch, LLC",
-    dateRange: "Summer 2018",
-    description: [
-      "Updated existing application to optimize end user accessibility, converting QR code scanner via Swift and installing new data features that import contact information and produce personalized QR codes.",
-      "Developed new code for existing applications to streamline operational functions while decreasing deployment time, successfully increasing application downloads by 1K+ after launch. ",
-    ],
-    tech: [
-      { name: "React Native", icon: Icons.REACT },
-      { name: "XCode (iOS)", icon: Icons.XCODE },
-      { name: "ObjC", icon: Icons.OBJC },
-      { name: "Swift", icon: Icons.SWIFT },
-    ],
-    location: "Mount Prospect, Illinois",
-  },
-  {
-    title: "Web Application Developer Intern",
-    companyName: "Robert Bosch, LLC",
-    dateRange: "Summer 2019 - May 2020",
-    description: [
-      "Utilized React JS library and React Native open-source mobile application framework to program and develop internal web applications that support operational efficiency across global branches.",
-      "Facilitated in-depth research to advance progress of new virtual reality (VR) mobile applications for internal use, conducting trial and error testing and quickly isolating technical limitations.",
-    ],
-    tech: [
-      { name: "React", icon: Icons.REACT },
-      { name: "React Native", icon: Icons.REACT },
-      { name: "MaterialUI", icon: Icons.MUI },
-      { name: "HTML", icon: Icons.HTML },
-      { name: "CSS", icon: Icons.CSS },
-      { name: "JavaScript", icon: Icons.JS },
-      { name: "TypeScript", icon: Icons.TYPESCRIPT },
-    ],
-    location: "Chicago | Mount Prospect, Illinois",
-  },
-  {
     title: "Front End Developer",
     companyName: "Investor Cash Management",
     dateRange: "Spring 2021 - Present",
@@ -59,5 +24,40 @@ export const experienceData: ExperienceData[] = [
       { name: "TypeScript", icon: Icons.TYPESCRIPT },
     ],
     location: "Remote | Wilmington, Delaware",
+  },
+  {
+    title: "Web Application Developer Intern",
+    companyName: "Robert Bosch, LLC",
+    dateRange: "Summer 2019 - May 2020",
+    description: [
+      "Utilized React JS library and React Native open-source mobile application framework to program and develop internal web applications that support operational efficiency across global branches.",
+      "Facilitated in-depth research to advance progress of new virtual reality (VR) mobile applications for internal use, conducting trial and error testing and quickly isolating technical limitations.",
+    ],
+    tech: [
+      { name: "React", icon: Icons.REACT },
+      { name: "React Native", icon: Icons.REACT },
+      { name: "MaterialUI", icon: Icons.MUI },
+      { name: "HTML", icon: Icons.HTML },
+      { name: "CSS", icon: Icons.CSS },
+      { name: "JavaScript", icon: Icons.JS },
+      { name: "TypeScript", icon: Icons.TYPESCRIPT },
+    ],
+    location: "Chicago | Mount Prospect, Illinois",
+  },
+  {
+    title: "Mobile Application Developer Intern",
+    companyName: "Robert Bosch, LLC",
+    dateRange: "Summer 2018",
+    description: [
+      "Updated existing application to optimize end user accessibility, converting QR code scanner via Swift and installing new data features that import contact information and produce personalized QR codes.",
+      "Developed new code for existing applications to streamline operational functions while decreasing deployment time, successfully increasing application downloads by 1K+ after launch. ",
+    ],
+    tech: [
+      { name: "React Native", icon: Icons.REACT },
+      { name: "XCode (iOS)", icon: Icons.XCODE },
+      { name: "ObjC", icon: Icons.OBJC },
+      { name: "Swift", icon: Icons.SWIFT },
+    ],
+    location: "Mount Prospect, Illinois",
   },
 ];
